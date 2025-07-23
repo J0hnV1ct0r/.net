7801bbedcb81672ce2ea8c9f77710babdb4cf887
João Victo de S. Alb. <j1747victor@gmail.com>
2025-07-23 13:37:15 -0300

    inicio da classe

---
+++ models/Pessoa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fundamentos_net.models
+{
+    public class Pessoa
+    {
+        public string name { get; set; }
+        public int idade { get; set; }
+    }
+}
